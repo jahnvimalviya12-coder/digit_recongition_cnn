127caed4379e5799e59ea919b43008ccca5b3422
jahnvimalviya12-coder <jahnvimalviya12@gmail.com>
2026-07-09 20:26:30 +0530

    Update cnn_app.py

--- cnn_app.py
+++ cnn_app.py
@@ -9,12 +9,12 @@ model = tf.keras.models.load_model("mnist_cnn.h5")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('L')  # Convert to grayscale
-        image = ImageOps.invert(image)  # Invert to match MNIST format
-        image = image.resize((28, 28))
-        img_array = np.array(image).reshape(-1, 28, 28, 1) / 255.0
-        st.image(image, caption="Uploaded Image", width=150)
+    image = ImageOps.invert(image)  # Invert to match MNIST format
+    image = image.resize((28, 28))
+    img_array = np.array(image).reshape(-1, 28, 28, 1) / 255.0
+    st.image(image, caption="Uploaded Image", width=150)
 
-        prediction = model.predict(img_array)
-        st.success(f"Predicted Digit: {np.argmax(prediction)}")
+    prediction = model.predict(img_array)
+    st.success(f"Predicted Digit: {np.argmax(prediction)}")
 
         
